1f448ed083d3bfbb5cd978c49e2b3782ba4b6b4c
petecarapetyan <pete@couldbe.net>
2018-03-15 08:42:31 -0500

    generates pages only on topicType direction

--- src/main/java/pull/camel/PullRoutes.java
+++ src/main/java/pull/camel/PullRoutes.java
@@ -4,31 +4,37 @@ import org.apache.camel.builder.RouteBuilder;
 import org.springframework.context.ApplicationContext;
 
 public class PullRoutes extends RouteBuilder {
-	ApplicationContext applicationContext;
-	
-	public PullRoutes(ApplicationContext applicationContext){
-		super();
-		this.applicationContext = applicationContext;
-	}
+    ApplicationContext applicationContext;
 
-	@Override
-	public void configure() throws Exception {
+    public PullRoutes(ApplicationContext applicationContext) {
+        super();
+        this.applicationContext = applicationContext;
+    }
 
-		from("timer://barebones?fixedRate=true&period=360000").log("\n\n\t .... running  ........");
+    @Override
+    public void configure() throws Exception {
 
-		from("file://propertyFiles/?noop=true").setHeader("fileKey").simple("${file:name.noext}")
-				.process(new VelocityHeaderPropertiesProcessor()).wireTap("direct:a").wireTap("direct:to-a")
-				.wireTap("direct:b").wireTap("direct:to-b")
-				.log("VELOCITY CREATED ${file:name.noext} TOPIC");
+        from("timer://barebones?fixedRate=true&period=360000").log("\n\n\t .... running  ........");
 
-		from("direct:a").to("velocity://velocity/a.html.vm")
-				.to("file:./../infoclouddancer/public?fileName=z${header.fileKey}c.html");
-		from("direct:to-a").to("velocity://velocity/to-a.html.vm")
-				.to("file:./../infoclouddancer/public/src?fileName=to-z${header.fileKey}c.html");
-		from("direct:b").to("velocity://velocity/b.html.vm")
-				.to("file:./../infoclouddancer/public?fileName=z${header.fileKey}e.html");
-		from("direct:to-b").to("velocity://velocity/to-b.html.vm")
-				.to("file:./../infoclouddancer/public/src?fileName=to-z${header.fileKey}e.html");
+        from("file://propertyFiles/?noop=true").setHeader("fileKey").simple("${file:name.noext}")
+                .process(new VelocityHeaderPropertiesProcessor()).wireTap("direct:a").wireTap("direct:to-a")
+                .wireTap("direct:b").wireTap("direct:to-b")
+                .log("VELOCITY CREATED ${file:name.noext} TOPIC");
 
-	}
+        from("direct:a")
+                .choice().when(header("topicType").regex("(Single Web Page|Dual Web Page)"))
+                .to("velocity://velocity/a.html.vm")
+                .to("file:./../infoclouddancer/public?fileName=z${header.fileKey}c.html");
+        from("direct:to-a")
+                .choice().when(header("topicType").regex("(Single Web Page|Dual Web Page)"))
+                .to("velocity://velocity/to-a.html.vm")
+                .to("file:./../infoclouddancer/public/src?fileName=to-z${header.fileKey}c.html");
+        from("direct:b").choice().when(header("topicType").regex("(Dual Web Page)"))
+                .to("velocity://velocity/b.html.vm")
+                .to("file:./../infoclouddancer/public?fileName=z${header.fileKey}e.html");
+        from("direct:to-b").choice().when(header("topicType").regex("(Dual Web Page)"))
+                .to("velocity://velocity/to-b.html.vm")
+                .to("file:./../infoclouddancer/public/src?fileName=to-z${header.fileKey}e.html");
+
+    }
 }
