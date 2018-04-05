d3ebed3a79c52f61ae3da39c10bea7742b197e1f
petecarapetyan <pete@couldbe.net>
2018-04-05 15:52:17 -0500

    fixed broken compile

--- src/test/java/pull/service/WriteTopicToPropsTest.java
+++ src/test/java/pull/service/WriteTopicToPropsTest.java
@@ -31,7 +31,7 @@ public class WriteTopicToPropsTest {
 		Topic topic = new Topic("topicType", "name", "category", 1234567890, "aHeadline",
 				"aPre", "aVideo", "aPost", "bHeadline", "bPre", "bVideo",
 				"bPost");
-		Properties properties = new WriteTopicToProps().parseTopicIntoProperties(topic);
+		Properties properties = new WriteTopicToProps().parseTopicIntoProperties(topic, true);
 		assertEquals(properties.getProperty("aPre"), "aPre");
 	}
 
