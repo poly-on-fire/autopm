a84e88381ea89205bce35b05c6dd14417a00fde3
petecarapetyan <pete@couldbe.net>
2018-08-11 19:47:36 -0500

    testing by adding more functionality back in

--- src/main/java/pull/service/DailyEmail.java
+++ src/main/java/pull/service/DailyEmail.java
@@ -28,25 +28,25 @@ public class DailyEmail {
      */
 
     public void send() {
-        System.out.println("SOMETHING SHOULD HAVE HAPPENED to "+ topicDayService.getEmailStarts().values().size());
+        System.out.println("SOMETHING SHOULD HAVE HAPPENED to at least "+ topicDayService.getEmailStarts().values().size());
 
-//        /*
-//         * This should seem confusing because it is getting a list of emails for
-//         * one day for one topic for one email address. Although this is
-//         * NEVER OK to have more than one email per topic per email address,
-//         * it is still physically possible, so that is what has to be done.
-//         */
-//        topicDayService.printTopicDaysMap();
-//        /* so this next line gets a de-duped list, that's about it*/
-//        Collection<EmailStart> emailStarts = (Collection<EmailStart>) topicDayService.getEmailStarts().values();
-//        for (EmailStart emailStart : emailStarts) {
-//            List<EmailOut> emailsOut = topicDayService.todaysEmail(emailStart, LocalDate.now());
-//            for (EmailOut emailOut : emailsOut) {
-////                new FetchSendEmailRepo(emailFrom, emailOut, emailStart, sendEmail);
-//                System.out.println(" emailOut emailStart.emailAddress  "+ emailOut + " " + emailOut.getEmailAddress());
-//            }
-//
-//        }
+        /*
+         * This should seem confusing because it is getting a list of emails for
+         * one day for one topic for one email address. Although this is
+         * NEVER OK to have more than one email per topic per email address,
+         * it is still physically possible, so that is what has to be done.
+         */
+        topicDayService.printTopicDaysMap();
+        /* so this next line gets a de-duped list, that's about it*/
+        Collection<EmailStart> emailStarts = (Collection<EmailStart>) topicDayService.getEmailStarts().values();
+        for (EmailStart emailStart : emailStarts) {
+            List<EmailOut> emailsOut = topicDayService.todaysEmail(emailStart, LocalDate.now());
+            for (EmailOut emailOut : emailsOut) {
+//                new FetchSendEmailRepo(emailFrom, emailOut, emailStart, sendEmail);
+                System.out.println(" emailOut emailStart.emailAddress  "+ emailOut + " " + emailOut.getEmailAddress());
+            }
+
+        }
     }
 
 }
