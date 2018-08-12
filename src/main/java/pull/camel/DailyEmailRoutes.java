01dc678e209e2034c53f3e0b8caf92ef61df646d
petecarapetyan <pete@couldbe.net>
2018-08-11 19:36:56 -0500

    attempting to get surrogate email ticket to work

--- src/main/java/pull/camel/DailyEmailRoutes.java
+++ src/main/java/pull/camel/DailyEmailRoutes.java
@@ -22,12 +22,9 @@ public class DailyEmailRoutes extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 //		from("quartz2://dailyEmail/myTimerName?fireNow").bean(dailyEmail);
-//		from("quartz2://dailyEmail/myTimerName?cron=0+0/1+*+?+*+*").bean(dailyEmail);
+		from("quartz2://dailyEmail/myTimerName?cron=0+0/1+*+?+*+*").bean(dailyEmail);
 //		from("quartz2://dailyEmail/friday?cron=0+18+6+?+*+FRI").bean(dailyEmail);
 
-		//to run every 5 minutes
-		from("quartz2://dailyEmail/monday?cron=0+0/5+*+*+*+?").bean(dailyEmail);
-		
 //		from("quartz2://dailyEmail/monday?cron=0+15+12+?+*+MON").bean(dailyEmail);
 //		from("quartz2://dailyEmail/tuesday?cron=0+5+11+?+*+TUE").bean(dailyEmail);
 //		from("quartz2://dailyEmail/wednesday?cron=0+45+9+?+*+WED").bean(dailyEmail);
